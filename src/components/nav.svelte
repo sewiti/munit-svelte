c5ec4317e49da9cdb10065d7e61d9a784bc7e57c
Mindaugas Bernotas <mindaugas.bernotas@protonmail.com>
2021-12-15 01:34:02 +0200

    Add dropdown menu (absolute trash)
    Absolute trash yet a requirement for an assignment.

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -2,6 +2,14 @@
   import { Link } from "svelte-navigator";
   import { token } from "$src/stores/auth";
   import { MenuIcon } from "svelte-feather-icons";
+
+  let dropdown = false;
+  const toggleDropdown = () => {
+    dropdown = !dropdown;
+  };
+  const hideDropdown = () => {
+    dropdown = false;
+  };
 </script>
 
 <nav class="container">
@@ -17,7 +25,8 @@
   </ul>
 
   {#if $token === ""}
-    <ul>
+    <!-- Logged in -->
+    <ul class="notXS">
       <li>
         <Link to="/register">Register</Link>
       </li>
@@ -25,8 +34,27 @@
         <Link to="/login">Login</Link>
       </li>
     </ul>
+    <!-- Logged in dropdown (this is trash) -->
+    <ul class="xs">
+      <div class="dropdown-container" hidden={!dropdown}>
+        <article class="dropdown">
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </article>
+      </div>
+      <li>
+        <button
+          class="primary outline"
+          style="padding:0.5em"
+          on:click={toggleDropdown}
+        >
+          <MenuIcon size="24" />
+        </button>
+      </li>
+    </ul>
   {:else}
-    <ul>
+    <!-- Guest -->
+    <ul class="notXS">
       <li>
         <Link to="/projects">My Projects</Link>
       </li>
@@ -35,6 +63,25 @@
       </li>
       <li>
         <Link to="/logout">Logout</Link>
+      </li>
+    </ul>
+    <!-- Guest dropdown (this is trash) -->
+    <ul class="xs">
+      <div class="dropdown-container" hidden={!dropdown}>
+        <article class="dropdown">
+          <Link to="/projects">My Projects</Link>
+          <Link to="/profile">My Profile</Link>
+          <Link to="/logout">Logout</Link>
+        </article>
+      </div>
+      <li>
+        <button
+          class="primary outline"
+          style="padding:0.5em"
+          on:click={toggleDropdown}
+        >
+          <MenuIcon size="24" />
+        </button>
       </li>
     </ul>
   {/if}
@@ -46,5 +93,33 @@
     font-family: "Coolvetica", var(--font-family);
     font-size: x-large;
     line-height: 1em;
+  }
+
+  .dropdown-container {
+    position: fixed;
+    width: 0;
+    height: 0;
+    z-index: 1;
+  }
+  .dropdown {
+    position: relative;
+    top: 3em;
+    right: 7em;
+    width: 10em;
+  }
+
+  .xs {
+    display: none;
+  }
+  .notXS {
+    display: block;
+  }
+  @media (max-width: 576px) {
+    .xs {
+      display: block;
+    }
+    .notXS {
+      display: none;
+    }
   }
 </style>
